6ca7fa6b28b118afcdc43b8c7db9ef9534d1c997
NghiNV <nghinv@lumi.biz>
2022-01-28 11:31:26 +0700

    Initial Commit

---
+++ error-page/error_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  error_pageApp.swift
+//  error-page
+//
+//  Created by NghiNV on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct error_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
